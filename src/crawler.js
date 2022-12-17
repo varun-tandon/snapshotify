3248cd55a5b84783e8b9a47994dc591025f81ce5
Varun Tandon <vrntandon@gmail.com>
2022-12-17 22:33:53 +0000

    Play with crawler options

--- src/crawler.js
+++ src/crawler.js
@@ -14,8 +14,8 @@ module.exports = async ({ paths, root, config }) => {
     })
     .filter(l => !enqueued.includes(l))   // Ensure we haven't seen it before
     .map(l => { enqueued.push(l); return l; }); // Remember, and return
-
-  const browser = await puppeteer.launch({ headless: true });
+aaa
+  const browser = await puppeteer.launch({ headless: true, pipe: true });
 
   const queue = cq().limit({ concurrency: 10 }).process(async path => {
     console.log("Processing", path);
